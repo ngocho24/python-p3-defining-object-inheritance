4b12a78b65e7b5ca936fb6ee78e524f57cc836d8
Elijah Ngocho <elijahngocho24@gmail.com>
2023-12-01 12:54:57 +0300

    completed

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,17 @@
 from vehicle import Vehicle
 
+class Vehicle:
+
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return "filling up!"
+
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
